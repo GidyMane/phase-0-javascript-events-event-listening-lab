50f87120a1a4ba39260b24360c01295dc64b1cce
GidyMane <muragegideon2000@gmail.com>
2023-10-07 13:25:29 +0300

    Add event listener

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function addingEventListener() {
-}
+ function addingEventListener() {
+
+const input = document.getElementById('input');
+ 
+  function clickAlert(){
+    alert('I was clicked')
+  }
+input.addEventListener('click', clickAlert);
+ }
+ addingEventListener()
+// const input = document.getElementById('input');
+
+// function clickAlert(){
+//     alert('I was clicked')
+// }
+// input.addEventListener('click', clickAlert);
